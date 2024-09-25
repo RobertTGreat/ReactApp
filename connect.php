6cab5ad15dec3ec79d2a207e95fd6e9b7ee5301b
Robert <111189845+RobertTGreat@users.noreply.github.com>
2024-09-25 12:41:26 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+//Set variables for the Server Name / Address, Database Name and Username & Password details.
+$my_host = "localhost";
+$my_db = 'bikeking';  //change to match your DB
+$my_db_username = "root";
+$my_db_passwd = "";
+
+try {
+    //PHP Data Objects. PDO is a lean, consistent way to access databases
+	$DB = new PDO("mysql:host=$my_host;dbname=$my_db", $my_db_username, $my_db_passwd);
+	
+	if($DB){
+        
+        echo "Success! Result Found";
+
+	}else{
+		echo "Database connection can not be established.";
+	}
+
+} catch (Exception $ex) {
+	echo $ex->getMessage();
+}
